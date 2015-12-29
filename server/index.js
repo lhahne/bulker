b377c31bc5a584ae978fdf5f3255ed2331420be9
Lauri Hahne <lauri.hahne@gofore.com>
2015-12-29 14:31:52 +0200

    trivial server implementation

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-console.log('it works');
+import * as restify from 'restify';
+
+const server = restify.createServer();
+
+server.get('/', (req, res, next) => {
+   res.send('hi');
+    next();
+});
+
+const port = 9000;
+
+server.listen(port, () => {
+    console.log(`${server.name} listening at ${server.url}`);
+});
