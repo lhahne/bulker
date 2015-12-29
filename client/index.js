ba1650deffd09918403326854b5430b296fdb927
Lauri Hahne <lauri.hahne@gofore.com>
2015-12-29 21:16:21 +0200

    reline

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 System.import('angular@1.4.8').then(function(angular) {
     console.log('loaded');
-    
+
     angular.module('bulkken', [])
         .controller('bulkController', function($scope, $http) {
             $http.get('/data').then(function(success) {
