99a32c2199440438565cfb053d38a9f4c827f29b
Lauri Hahne <lauri.hahne@gofore.com>
2015-12-29 16:58:20 +0200

    use express and passport

--- server/index.js
+++ server/index.js
@@ -1,11 +1,49 @@
-import * as restify from 'restify';
+const passport = require('passport');
+const express = require('express');
 
-const server = restify.createServer();
+const FitbitStrategy = require( 'passport-fitbit-oauth2' ).FitbitOAuth2Strategy;;
+
+
+const server = express();
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+server.use(passport.initialize());
+
+passport.use(new FitbitStrategy({
+        clientID:     process.env.ID,
+        clientSecret: process.env.SECRET,
+        callbackURL: "http://localhost:9000/auth/example/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        done(null, profile);
+    }
+));
+
 
 server.get('/', (req, res, next) => {
    res.send('hi');
     next();
 });
+
+server.get('/auth/example',
+    passport.authenticate('fitbit', {scope: ['weight', 'profile']}));
+
+server.get('/auth/example/callback',
+    passport.authenticate('fitbit', { failureRedirect: '/login' }),
+    function(req, res) {
+        // Successful authentication, redirect home.
+        console.log('at callback callback');
+        console.log(req);
+        res.redirect('/');
+    }
+);
 
 const port = 9000;
 
