5d2f9c49fde98ff47bf340c7b7a3f5fc89b0bfa7
Lauri Hahne <lauri.hahne@gofore.com>
2015-12-30 12:36:44 +0200

    display weight data on page

--- client/index.js
+++ client/index.js
@@ -1,10 +1,41 @@
 const rx = require('rx');
-const axios = require('axios')
+const axios = require('axios');
+
+const React = require('react');
+const ReactDOM = require('react-dom');
 
 const token = rx.Observable.fromPromise(axios.get('/user'))
     .map(response => response.data.token);
 
-const profile = token.flatMap(token => rx.Observable.fromPromise(axios.get('https://api.fitbit.com/1/user/-/profile.json', {headers: {Authorization: 'Bearer ' + token}})))
-    .map(response => response.data.user)
-    .subscribe(v => console.log(v));
+const get = path => token.flatMap(token => rx.Observable.fromPromise(axios.get(`https://api.fitbit.com${path}`, {headers: {Authorization: 'Bearer ' + token}})))
 
+const profile = get('/1/user/-/profile.json')
+    .map(response => response.data.user);
+
+const today = new Date().toISOString().substr(0, 10);
+
+const weightData = get(`/1/user/-/body/log/weight/date/${today}.json`)
+    .map(response => response.data.weight[0]);
+
+const DataView = React.createClass({
+    getInitialState() {
+        return {};
+    },
+    componentDidMount() {
+        weightData.subscribe(data => this.setState({data}));
+    },
+    render() {
+        const displayData = data => {
+            if (data)
+             return <p>Weight today {this.state.data.weight}</p>;
+        }
+
+        return (
+            <div>
+                {displayData(this.state.data)}
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(<DataView />, document.getElementById('root'));
