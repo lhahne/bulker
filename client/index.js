db32a376875b5f411f85f2e7ccb354008c7535d1
Lauri Hahne <lauri.hahne@gofore.com>
2015-12-30 13:35:09 +0200

    namingConvention$

--- client/index.js
+++ client/index.js
@@ -6,17 +6,17 @@ const ReactDOM = require('react-dom');
 
 const log = require('./logger');
 
-const token = rx.Observable.fromPromise(axios.get('/user'))
+const token$ = rx.Observable.fromPromise(axios.get('/user'))
     .map(response => response.data.token);
 
-const get = path => token.flatMap(token => rx.Observable.fromPromise(axios.get(`https://api.fitbit.com${path}`, {headers: {Authorization: 'Bearer ' + token}})));
+const get$ = path => token$.flatMap(token => rx.Observable.fromPromise(axios.get(`https://api.fitbit.com${path}`, {headers: {Authorization: 'Bearer ' + token}})));
 
-const profile = get('/1/user/-/profile.json')
+const profile$ = get$('/1/user/-/profile.json')
     .map(response => response.data.user);
 
 const today = new Date().toISOString().substr(0, 10);
 
-const weightData = get(`/1/user/-/body/log/weight/date/${today}.json`)
+const weightData$ = get$(`/1/user/-/body/log/weight/date/${today}.json`)
     .map(response => response.data.weight[0]);
 
 const DataView = React.createClass({
@@ -24,7 +24,7 @@ const DataView = React.createClass({
         return {};
     },
     componentDidMount() {
-        weightData.subscribe(data => {
+        weightData$.subscribe(data => {
             log.info(data);
             this.setState({data})
         });
